ffcb8a6f89ef892a05bdf471a0977e789c9eb80d
Enes <70810472+ahmetenesdur@users.noreply.github.com>
2023-04-04 04:45:34 +0300

    Creating Request Api Key

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,10 @@
-import { FC } from "react";
-
 import type { Metadata } from "next";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
+import { notFound } from "next/navigation";
+import { db } from "@/lib/db";
+import RequestApiKey from "@/components/RequestApiKey";
+import ApiDashboard from "@/components/ApiDashboard";
 
 export const metadata: Metadata = {
   title: "Similarity API | Dashboard",
@@ -11,9 +13,17 @@ export const metadata: Metadata = {
 
 const page = async () => {
   const user = await getServerSession(authOptions);
-  if (!user) return <p>Not logged in</p>;
+  if (!user) return notFound();
 
-  return <div>page</div>;
+  const apiKey = await db.apiKey.findFirst({
+    where: { userId: user.user.id, enabled: true },
+  });
+
+  return (
+    <div className="max-w-7xl mx-auto mt-16">
+      {apiKey ? <ApiDashboard /> : <RequestApiKey />}
+    </div>
+  );
 };
 
 export default page;
